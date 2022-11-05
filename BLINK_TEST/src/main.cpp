fb500866d28140a1cecb8fd8b7eecea915b4246f
1998-felix <felix.gateru@gmail.com>
2022-11-05 18:54:03 +0300

    Successfully blink onboard led using dfu

--- BLINK_TEST/src/main.cpp
+++ BLINK_TEST/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#define DELAY 100
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(PC13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(PC13,HIGH);
+  delay(DELAY);
+  digitalWrite(PC13,LOW);
+  delay(DELAY);
 }
